aeaba497661622e732c43b2e653e130a312b91fa
hjylewis <hlewis@uchicago.edu>
2015-03-15 11:38:05 -0500

    Add more responsiveness for user without organizations

--- public/script/login.js
+++ public/script/login.js
@@ -10,25 +10,30 @@ $.getJSON("orgs.json")
   })
   .fail();
 var render = function (orgs) {
-  document.getElementById('user').addEventListener("click", userClick, false);
-  function userClick(){
+  document.getElementById('user').addEventListener("click", function userClick(){
     document.getElementById('orgdrop').disabled = "true";
     document.getElementById('publictext').disabled = "true";
-  }
+  }, false);
+  
 
-  document.getElementById('org').addEventListener("click", orgClick, false);
-  function orgClick(){
-    document.getElementById('orgdrop').disabled = null;
-    document.getElementById('publictext').disabled = "true";
-  }
-  document.getElementById('public').addEventListener("click", pubClick, false);
-  function pubClick(){
+  document.getElementById('public').addEventListener("click", function pubClick(){
     document.getElementById('publictext').disabled = null;
     document.getElementById('orgdrop').disabled = "true";
-  }
+  }, false);
+  
+  
+  if (orgs.length !== 0){
+    document.getElementById('org').addEventListener("click", function orgClick(){
+      document.getElementById('orgdrop').disabled = null;
+      document.getElementById('publictext').disabled = "true";
+    }, false);
 
-  orgs.forEach(function (org) {
-    d3.select('select').append('option').append('text').text(org);
-  });
+    orgs.forEach(function (org) {
+      d3.select('select').append('option').append('text').text(org);
+    });
+  } else {
+    document.getElementById('org').disabled = "true";
+    d3.select('select').append('option').append('text').text("User has no Orgs");
+  }
 
 };
